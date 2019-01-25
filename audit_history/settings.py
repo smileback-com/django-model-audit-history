28cb53fe73929d66777d7479184465525194fd3f
Ilin <serhiy.ilin@innoteka.com>
2019-01-25 18:34:05 +0200

    Added template for AuditHistoryWidget

--- audit_history/settings.py
+++ audit_history/settings.py
@@ -1,6 +1,2 @@
-HISTORY_FIELD_VIEW = '<tr>' \
-                         '<td>{timestamp:%Y-%m-%d %H:%M:%S} UTC</td><td>{actor}</td>' \
-                         '<td>{event}</td><td>{payload}</td>' \
-                     '</tr>'
 DEFAULT_ACTOR = 'System'
 TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%f+00:00'
--- audit_history/widgets.py
+++ audit_history/widgets.py
@@ -2,30 +2,31 @@ import datetime
 
 from django.forms.widgets import Widget
 from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
 
 from .settings import (
-    HISTORY_FIELD_VIEW,
     TIMESTAMP_FORMAT,
     DEFAULT_ACTOR
 )
 
 
 class AuditHistoryWidget(Widget):
+    template_name = 'widget.html'
+
     def render(self, name, value, attrs=None, renderer=None):
         if isinstance(value, list):
             history = reversed(value)
-            rows = [HISTORY_FIELD_VIEW.format(
-                        timestamp=datetime.datetime.strptime(
+            rows = [{
+                        'timestamp': datetime.datetime.strptime(
                             entry.get('timestamp', '1900-01-01T00:00:00.000+00:00'),
                             TIMESTAMP_FORMAT
                         ),
-                        actor=entry['actor'].get('email', '[n/a]') if entry.get('actor') else DEFAULT_ACTOR,
-                        event=entry.get('event', '[n/a]'),
-                        payload='<br>'.join(['%s=%s' % (k, v)
-                                             for k, v in sorted(entry.items())
-                                             if k not in ('timestamp', 'actor', 'event',)])
-                    ) for entry in history]
+                        'actor': entry['actor'].get('email', '[n/a]') if entry.get('actor') else DEFAULT_ACTOR,
+                        'event': entry.get('event', '[n/a]'),
+                        'payload':['%s=%s' % (k, v) for k, v in sorted(entry.items())
+                                   if k not in ('timestamp', 'actor', 'event',)]
+                        } for entry in history]
 
-            return mark_safe('<table>' + ''.join(rows) + '</table>')
+            return mark_safe(render_to_string(self.template_name, {'data': rows}))
 
         raise Exception('Unexpected type to render: ' + str(type(value)))
